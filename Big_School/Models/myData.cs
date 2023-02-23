afc6caf65cd802378e4782809285a63073b91920
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-02-23 09:24:41 +0700

    Add project files.

---
+++ Big_School/Models/myData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Big_School.Models
+{
+    public class myData
+    {
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext()
+            {
+
+            }
+        }
+    }
+}
